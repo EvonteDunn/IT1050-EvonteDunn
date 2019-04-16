a0547d46319d42d52645d98e212889418dc62c68
dunnevonte@gmail.com <dunnevonte@gmail.com>
2019-04-15 22:07:58 -0400

    Lab 4 ,_2 and _3 have all the code for the following lab assignment

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                {
+                    if ((i % 2) == 0)
+                    { Console.WriteLine("Number is even"); }
+                    else if ((i % 2) != 0)
+                    {
+                        Console.WriteLine("Number is odd");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+//1.
+//A)Control variable - A variable used to control the number of times a set of statements will execute
+//B)Initial value – The starting value assigned to the control variable
+//C)The increment/decrement – Modifies the control variable in either an increase or a decrease during the loop
+//D)The loop-continuation condition - Determines whether to continue looping.
+
+//2.As long as a loop-continuation condition remains true, the While and For repetition statements both will repeatedly execute a statement or set of statements. The while and for repetition statements both also implement their bodies zero or more times. The control variable in a While statement is usually initialized before the loop and is incremented within the loop's body. 
+
+//3. It would be useful to utilize a do-while statement when something needs to occur, but it won't occur correctly with certainty on the first iteration. The loop will always run at least once with a do-while statement, so if it is preferred that a condition be checked at the end of the loop execution it will verify if it should iterate further.
---
+++ Lab4_03/Lab4_03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 9;
+            while (i < 20)
+            {
+                i++;
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+
+            for (int b = 0; b < 101; b++)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine("********");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+    }
+
+
